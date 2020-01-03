43a3752a96374ffb8e4231855c6fa0d487f1c4fc
Ian <qwe2795qwe@gmail.com>
2020-01-03 18:04:15 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vueshopping/dist/',//編譯後的路徑
+    assetsPublicPath: '/view/',//編譯後的路徑
     proxyTable: {},
 
     // Various Dev Server settings
