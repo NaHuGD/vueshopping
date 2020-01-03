8fec5a24ffec559d4db1a04cda9299b08922f4f7
Ian <qwe2795qwe@gmail.com>
2020-01-03 14:09:41 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <HelloWorld/> -->
     <router-view></router-view>
   </div>
   
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: 'App',
-  // components:{
-  //   HelloWorld
-  // },
-  // created(){
-  //   const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`//'https://vue-course-api.hexschool.io/api/apple/products';
-  //   console.log(process.env.APIPATH,process.env.CUSTOMPATH)
-  //   this.$http.get(api).then((response) => {
-  //     console.log(response.data)
-  //   })
-  // },
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from '@/components/HelloWorld';
 import Admin from '@/components/Admin';
 import User from '@/components/user/User';
 import Login from '@/components/pages/Login';
