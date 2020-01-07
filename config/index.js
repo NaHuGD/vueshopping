a8b1553043838378fd7c91149083401cd02b266e
Ian <qwe2795qwe@gmail.com>
2020-01-07 13:39:59 +0800

    updata

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vueshopping/',
+    assetsPublicPath: '/vueshopping/dist/',
 
     /**
      * Source Maps
